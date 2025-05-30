9e08a8da12915045387c9ff542b67f5d47703f1c
Franco Enciso <encisofranco91@gmail.com>
2025-05-29 21:04:17 -0300

    Create gestionBandas.py

---
+++ gestionBandas.py
@@ -0,0 +1,36 @@
+def agregarBanda(nombreBanda, email, tarifa30Min, telefono, idBanda):
+    bandas[idBanda] = {
+        "activo": True,
+        "NombreBanda": nombreBanda,
+        "Email": email,
+        "Telefono": telefono,
+        "Tarifa30Min": tarifa30Min,
+        "Generos": {"genero1": genero[0],
+                    "genero2": genero[1]}
+    }
+    return bandas
+
+
+def modificarBanda(bandas, nombreBanda, email, tarifa30Min, telefono, idBanda):
+    
+    bandas[idBanda] = {
+        "NombreBanda": nombreBanda,
+        "Email": email,
+        "Tarifa30Min": tarifa30Min,
+        "Telefono"
+        "Generos": {"genero1": generos[0],
+                    "genero2": generos[1]}
+    }
+
+    return bandas
+
+def inactivarBanda(bandas,idBanda):
+    
+    bandas[idBanda]["activo"] = False
+    return bandas
+
+def listarBandasActivas(bandas):
+    for banda in bandas:
+        if bandas[banda]["activo"] == True:
+            print(f"IdBanda: {banda}")
+            print(bandas[banda])
